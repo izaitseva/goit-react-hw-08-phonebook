2a643094be080aad58fb381f0a5b5bbc5abee339
Iryna Zaitseva <zaitseva02@gmail.com>
2023-03-30 16:19:52 +0300

    basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { store, persistor } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/goit_hw_8">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
