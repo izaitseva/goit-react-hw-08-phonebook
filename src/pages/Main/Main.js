1a9c473d473413374a5fc18b0cc949fcffb21958
Iryna Zaitseva <zaitseva02@gmail.com>
2023-03-30 16:14:38 +0300

    header style

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,15 +1,32 @@
 import { Outlet } from "react-router"
 import { Navigation } from "../../navigation/Navigation"
-import styles from "./Main.modules.css"
+import styles from "./Main.module.css"
 
 import AuthLayout from "modules/Authlayout/AuthLayout"
+import { useSelector } from "react-redux"
+import { getUser, isUserLogin } from "redux/auth/auth-selector"
 
 export const Main = () => {
 
+    const { name } = useSelector(getUser);
+    const isLogin = useSelector(isUserLogin)
+
     return (
-        <AuthLayout className={styles.main_container}>
-            <Navigation />
-            <Outlet />
-        </AuthLayout>
+        <>
+            {!isLogin &&
+                <h1 className={`${styles.header} ${styles.auth_header}`}>Your phonebook</h1>
+            }
+
+            {isLogin &&
+                <h1 className={styles.header}>
+                    <span className={styles.user_name}>{name}'s </span>
+                    phonebook
+                </h1>
+            }
+            <AuthLayout className={styles.main_container}>
+                <Navigation />
+                <Outlet />
+            </AuthLayout>
+        </>
     )
 }
