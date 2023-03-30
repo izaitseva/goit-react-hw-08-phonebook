202e0f2d6016a5a102dcbfc492013d2d9da75e7d
Iryna Zaitseva <zaitseva02@gmail.com>
2023-03-30 15:02:03 +0300

    check errors

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist';
-import {
-    FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER
-} from 'redux-persist';
+import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
